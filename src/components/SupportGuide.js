cd67f5ebd254f61274aa1cafd28820a7deff69fe
shehbazjafri <galaxytwirls@gmail.com>
2021-10-29 01:02:32 +0530

    feat(app): move steps to config

--- src/components/SupportGuide.js
+++ src/components/SupportGuide.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Button from "@mui/material/Button";
 import Alert from "@mui/material/Alert";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { SUPPORT_STEPS } from "../config/questionConstants";
 
 const Container = styled.div`
   display: flex;
@@ -52,179 +53,13 @@ const StyledButton = styled(Button)`
   }
 `;
 
-const steps = [
-  {
-    step: 1,
-    text: "Welcome to SkyCom's iPhone Troubleshooting. \n\n To begin we will need the correct model you are using?",
-    lines: [
-      {
-        text: "Welcome to Guided iPhone Troubleshooting",
-      },
-      {
-        text: "To begin we will need the correct model you are using?",
-      },
-    ],
-    options: [
-      "iPhone 12",
-      "iPhone 11",
-      "iPhone XS",
-      "iPhone XR",
-      "iPhone XS Max",
-      "iPhone X",
-    ],
-  },
-  {
-    step: 2,
-    text: "<info>Thank you for the information. Please select the issue you are experiencing with your iPhone.</info>",
-    lines: [
-      {
-        text: "Thank you for the information. Please select the issue you are experiencing with your iPhone.",
-        type: "info",
-        props: {
-          variant: "filled",
-          severity: "info",
-          style: {
-            fontSize: "1.5rem",
-          },
-        },
-      },
-    ],
-    options: [
-      "iPhone is running slowly",
-      "iPhone does not connect to Wi-Fi",
-      "iPhone is not repsonding",
-      "iPhone screen/body is damaged",
-    ],
-  },
-  {
-    step: 3,
-    text: "First, please do the following: Clear your phone's browser cache. How do I clear my cache?  Go to Settings > Safari > Clear History and Website Data.If you just want to clear the website data collected, you’ll need to tap Settings > Safari > Advanced > Website Data > Remove all website data.",
-    lines: [
-      {
-        text: "First, please do the following: Clear your phone's browser cache.",
-      },
-      {
-        text: "How do I clear my cache?",
-      },
-      {
-        text: "Go to Settings > Safari > Clear History and Website Data.If you just want to clear the website data collected, you’ll need to tap Settings > Safari > Advanced > Website Data > Remove all website data.",
-      },
-      {
-        text: "Did that resolve your issue?",
-        type: "info",
-        props: {
-          variant: "filled",
-          severity: "info",
-          style: {
-            fontSize: "1.5rem",
-          },
-        },
-      },
-    ],
-    options: ["Yes", "No"],
-  },
-  {
-    step: 4,
-    text: "Next: Update your phone's OS. How do I update OS? Make sure your phone is plugged in for charging and is connected to WiFi. Go to Settings > General > Software Update.",
-    lines: [
-      {
-        text: "Next: Update your phone's OS.",
-      },
-      {
-        text: "How do I update OS?",
-      },
-      {
-        text: "Make sure your phone is plugged in for charging and is connected to WiFi.",
-      },
-      {
-        text: "Go to Settings > General > Software Update.",
-      },
-      {
-        text: "Does that resolve your issue?",
-        type: "info",
-        props: {
-          variant: "filled",
-          severity: "info",
-          style: {
-            fontSize: "1.5rem",
-          },
-        },
-      },
-    ],
-    options: ["Yes", "No"],
-  },
-  {
-    step: 5,
-    text: "Next: Delete apps that you don't use.Note: There are two ways to remove apps on an iPhone. Delete Apps – removes an app and all its data.Offload Apps – removes an app but keeps a backup of its data.How do I delete apps?Press and hold the app you want to delete and, once it begins to shake around on your screen, tap the “x” in the top left cornerAlternatively, you can go to Settings > General > iPhone Storage > Select app > Delete app.How do I offload apps?Go to Settings > General > iPhone Storage > Offload Unused Apps > Enable.",
-    lines: [
-      {
-        text: "Next: Delete apps that you don't use.",
-      },
-      {
-        text: "Note: There are two ways to remove apps on an iPhone.",
-      },
-      {
-        text: "Delete Apps – removes an app and all its data.",
-      },
-      {
-        text: "Offload Apps – removes an app but keeps a backup of its data.",
-      },
-      {
-        text: "How do I delete apps?",
-      },
-      {
-        text: "Press and hold the app you want to delete and, once it begins to shake around on your screen, tap the “x” in the top left corner.",
-      },
-      {
-        text: "Alternatively, you can go to Settings > General > iPhone Storage > Select app > Delete app.",
-      },
-      {
-        text: "How do I offload apps?",
-      },
-      {
-        text: "Go to Settings > General > iPhone Storage > Offload Unused Apps > Enable.",
-      },
-      {
-        text: "Does that resolve your issue?",
-        type: "info",
-        props: {
-          variant: "filled",
-          severity: "info",
-          style: {
-            fontSize: "1.5rem",
-          },
-        },
-      },
-    ],
-    options: ["Yes", "No"],
-  },
-  {
-    step: 6,
-    text: "",
-    lines: [],
-    options: ["Open a case"],
-  },
-  {
-    step: 7,
-    text: "That's great. We're glad that resolved your issue.",
-    lines: [
-      {
-        text: "That's great. We're glad that resolved your issue.",
-      },
-    ],
-    options: ["Done", "Troubleshoot other issues"],
-  },
-];
-
 const TypeToComponent = {
   info: Alert,
-  error: Alert,
-  success: Alert,
 };
 
 function SupportGuide({ step, handleOptionSelect, handleNext, handleBack }) {
   // render current step text and options as buttons
-  const currentStep = steps.find((s) => s.step === step);
+  const currentStep = SUPPORT_STEPS.find((s) => s.step === step);
   const { options, lines } = currentStep;
 
   // replace string in text with component using TypeToComponent
--- src/config/questionConstants.js
+++ src/config/questionConstants.js
@@ -1,27 +1,12 @@
-// export const SUPPORT_QUESTIONS = [
-//     {
-//         id: '1',
-//         question: " To get started, please select the correct iPhone model from the options below.",
-//         options: ["iPhone 12", "iPhone 11", "iPhone XS", "iPhone XS Max"],
-//     },
-//     {
-//         id: '2',
-
-// ]
-export const SUPPORT_QUESTIONS = [
+export const SUPPORT_STEPS = [
   {
-    id: "1",
     step: 1,
-    messages: [
+    lines: [
       {
-        id: "1",
-        text: "<h3>Welcome to iPhone troubleshooting</h3>",
-        type: "message",
+        text: "Welcome to Guided iPhone Troubleshooting",
       },
       {
-        id: "2",
-        text: "To get started, please select the correct iPhone model from the options below.",
-        type: "message",
+        text: "To begin we will need the correct model you are using?",
       },
     ],
     options: [
@@ -33,9 +18,139 @@ export const SUPPORT_QUESTIONS = [
       "iPhone X",
     ],
   },
+  {
+    step: 2,
+    lines: [
+      {
+        text: "Thank you for the information. Please select the issue you are experiencing with your iPhone.",
+        type: "info",
+        props: {
+          variant: "filled",
+          severity: "info",
+          style: {
+            fontSize: "1.5rem",
+          },
+        },
+      },
+    ],
+    options: [
+      "iPhone is running slowly",
+      "iPhone does not connect to Wi-Fi",
+      "iPhone is not repsonding",
+      "iPhone screen/body is damaged",
+    ],
+  },
+  {
+    step: 3,
+    lines: [
+      {
+        text: "First, please do the following: Clear your phone's browser cache.",
+      },
+      {
+        text: "How do I clear my cache?",
+      },
+      {
+        text: "Go to Settings > Safari > Clear History and Website Data.If you just want to clear the website data collected, you’ll need to tap Settings > Safari > Advanced > Website Data > Remove all website data.",
+      },
+      {
+        text: "Did that resolve your issue?",
+        type: "info",
+        props: {
+          variant: "filled",
+          severity: "info",
+          style: {
+            fontSize: "1.5rem",
+          },
+        },
+      },
+    ],
+    options: ["Yes", "No"],
+  },
+  {
+    step: 4,
+    lines: [
+      {
+        text: "Next: Update your phone's OS.",
+      },
+      {
+        text: "How do I update OS?",
+      },
+      {
+        text: "Make sure your phone is plugged in for charging and is connected to WiFi.",
+      },
+      {
+        text: "Go to Settings > General > Software Update.",
+      },
+      {
+        text: "Does that resolve your issue?",
+        type: "info",
+        props: {
+          variant: "filled",
+          severity: "info",
+          style: {
+            fontSize: "1.5rem",
+          },
+        },
+      },
+    ],
+    options: ["Yes", "No"],
+  },
+  {
+    step: 5,
+    lines: [
+      {
+        text: "Next: Delete apps that you don't use.",
+      },
+      {
+        text: "Note: There are two ways to remove apps on an iPhone.",
+      },
+      {
+        text: "Delete Apps – removes an app and all its data.",
+      },
+      {
+        text: "Offload Apps – removes an app but keeps a backup of its data.",
+      },
+      {
+        text: "How do I delete apps?",
+      },
+      {
+        text: "Press and hold the app you want to delete and, once it begins to shake around on your screen, tap the “x” in the top left corner.",
+      },
+      {
+        text: "Alternatively, you can go to Settings > General > iPhone Storage > Select app > Delete app.",
+      },
+      {
+        text: "How do I offload apps?",
+      },
+      {
+        text: "Go to Settings > General > iPhone Storage > Offload Unused Apps > Enable.",
+      },
+      {
+        text: "Does that resolve your issue?",
+        type: "info",
+        props: {
+          variant: "filled",
+          severity: "info",
+          style: {
+            fontSize: "1.5rem",
+          },
+        },
+      },
+    ],
+    options: ["Yes", "No"],
+  },
+  {
+    step: 6,
+    lines: [],
+    options: ["Open a case"],
+  },
+  {
+    step: 7,
+    lines: [
+      {
+        text: "That's great. We're glad that resolved your issue.",
+      },
+    ],
+    options: ["Done", "Troubleshoot other issues"],
+  },
 ];
-// {
-//     id: '2',
-//     text: "Thank you for the information. Please select the issue you are experiencing with your iPhone.",
-//     type: "info",
-//     options: ["iPhone is running slowly", "iPhone is not responding", "iPhone is not working", "iPhone is not charging"],
